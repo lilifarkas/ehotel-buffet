4305b604fc807d8fe84024d79c3d4b3076863eea
Dorian Szatmari <dorian.szatmari@gmail.com>
2022-11-21 21:13:31 +0100

    Added reservation manager and provider class

---
+++ CodeCool.EhotelBuffet/Reservations/Service/ReservationManager.cs
@@ -0,0 +1,22 @@
+﻿using CodeCool.EhotelBuffet.Guests.Model;
+using CodeCool.EhotelBuffet.Reservations.Model;
+
+namespace CodeCool.EhotelBuffet.Reservations.Service;
+
+public class ReservationManager : IReservationManager
+{
+    public void AddReservation(Reservation reservation)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Guest> GetGuestsForDate(DateTime date)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Reservation> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ CodeCool.EhotelBuffet/Reservations/Service/ReservationProvider.cs
@@ -0,0 +1,12 @@
+﻿using CodeCool.EhotelBuffet.Guests.Model;
+using CodeCool.EhotelBuffet.Reservations.Model;
+
+namespace CodeCool.EhotelBuffet.Reservations.Service;
+
+public class ReservationProvider : IReservationProvider
+{
+    public Reservation Provide(Guest guest, DateTime seasonStart, DateTime seasonEnd)
+    {
+        throw new NotImplementedException();
+    }
+}
