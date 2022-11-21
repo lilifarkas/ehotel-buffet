b429bc26084b9feeb68d579f5888e5e7e6143590
Dorian Szatmari <dorian.szatmari@gmail.com>
2022-11-21 21:39:59 +0100

    ReservationProvider complete.

--- CodeCool.EhotelBuffet/Reservations/Service/ReservationProvider.cs
+++ CodeCool.EhotelBuffet/Reservations/Service/ReservationProvider.cs
@@ -7,6 +7,16 @@ public class ReservationProvider : IReservationProvider
 {
     public Reservation Provide(Guest guest, DateTime seasonStart, DateTime seasonEnd)
     {
-        throw new NotImplementedException();
+        Random rnd = new Random();
+        //Generate a random start stay date
+        int stayStartDate = rnd.Next(seasonStart.Day, seasonEnd.Day - 1);
+        //Generate random stay length, stayStartDate + stayLength can't exceed seasonEnd.
+        int stayLength = rnd.Next(1, seasonEnd.Day - stayStartDate);
+        
+        //Create DateTime objects from calculated dates.
+        DateTime startDate = new DateTime(seasonStart.Year, seasonStart.Month, stayStartDate);
+        DateTime endDate = new DateTime(seasonStart.Year, seasonStart.Month, stayStartDate + stayLength);
+        
+        return new Reservation(startDate, endDate, guest);
     }
 }
