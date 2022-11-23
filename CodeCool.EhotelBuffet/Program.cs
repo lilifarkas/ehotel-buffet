0ed57e96343fac0e47097a712d000d30687836bd
Attila Dobi <attiladobi100@gmail.com>
2022-11-23 16:34:54 +0100

    Buffet Consume and CollectWaste method developed

--- CodeCool.EhotelBuffet/Program.cs
+++ CodeCool.EhotelBuffet/Program.cs
@@ -6,17 +6,18 @@ using CodeCool.EhotelBuffet.Reservations.Service;
 using CodeCool.EhotelBuffet.Simulator.Service;
 using CodeCool.EhotelBuffet.Ui;
 
-ITimeService timeService = new TimeService();
-IMenuProvider menuProvider = new MenuProvider();
-IRefillService refillService = null;
-IGuestGroupProvider guestGroupProvider = null;
-IReservationProvider reservationProvider = null;
-IReservationManager reservationManager = null;
+// ITimeService timeService = new TimeService();
+// IMenuProvider menuProvider = new MenuProvider();
+// IRefillService refillService = null;
+// IGuestGroupProvider guestGroupProvider = null;
+// IReservationProvider reservationProvider = null;
+// IReservationManager reservationManager = null;
+//
+// IBuffetService buffetService = new BuffetService(menuProvider, refillService);
+// IDiningSimulator diningSimulator =
+//     new BreakfastSimulator(buffetService, reservationManager, guestGroupProvider, timeService);
+//
+// EhoteBuffetUi ui = new EhoteBuffetUi(reservationProvider, reservationManager, diningSimulator);
+//
+// ui.Run();
 
-IBuffetService buffetService = new BuffetService(menuProvider, refillService);
-IDiningSimulator diningSimulator =
-    new BreakfastSimulator(buffetService, reservationManager, guestGroupProvider, timeService);
-
-EhoteBuffetUi ui = new EhoteBuffetUi(reservationProvider, reservationManager, diningSimulator);
-
-ui.Run();
